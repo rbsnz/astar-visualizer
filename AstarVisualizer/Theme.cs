2fd01b069ad4ca3efc84f2995331b97a176ae7b6
rob <109945998+rbsnz@users.noreply.github.com>
2022-10-22 01:50:04 +1300

    Add Theme class

---
+++ AstarVisualizer/Theme.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+using SFML.Graphics;
+
+namespace AstarVisualizer;
+
+internal static class Theme
+{
+    static Color FromHex(string hex)
+    {
+        if (string.IsNullOrWhiteSpace(hex))
+            throw new ArgumentException("Value cannot be empty.", nameof(hex));
+
+        if (hex.StartsWith('#'))
+            hex = hex[1..];
+
+        if (!uint.TryParse(hex, NumberStyles.HexNumber, CultureInfo.InvariantCulture, out uint value))
+            throw new ArgumentException("Not a valid hex color.", nameof(hex));
+
+        return new Color(value << 8 | 0xFF);
+    }
+
+    public static readonly Color Background = FromHex("1B2430");
+    public static readonly Color VertexFill = FromHex("D6D5A8");
+    public static readonly Color EdgeFill = FromHex("29C7AC");
+    public static readonly Color PotentialEdgeFill = FromHex("29C7AC");
+}

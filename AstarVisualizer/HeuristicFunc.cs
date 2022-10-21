2d12256f9971e1b7f9ecda822b6be4db3e57ce30
rob <109945998+rbsnz@users.noreply.github.com>
2022-10-22 01:50:05 +1300

    Add Heuristics/HeuristicFunc

---
+++ AstarVisualizer/HeuristicFunc.cs
@@ -0,0 +1,9 @@
+﻿namespace AstarVisualizer;
+
+/// <summary>
+/// Calculates a heuristic value for the current vertex given the goal vertex.
+/// </summary>
+/// <param name="current">The current vertex.</param>
+/// <param name="goal">The goal vertex.</param>
+/// <returns>The heuristic value for the current vertex.</returns>
+public delegate float HeuristicFunc(Vertex current, Vertex goal);
---
+++ AstarVisualizer/Heuristics.cs
@@ -0,0 +1,17 @@
+﻿namespace AstarVisualizer;
+
+/// <summary>
+/// Defines heuristic functions between two vertices.
+/// </summary>
+public static class Heuristics
+{
+    /// <summary>
+    /// Returns zero.
+    /// </summary>
+    public static readonly HeuristicFunc None = (current, goal) => 0;
+
+    /// <summary>
+    /// Returns the Euclidean distance between the current and goal vertices.
+    /// </summary>
+    public static readonly HeuristicFunc Euclidean = (current, goal) => Maths.Distance(current.Position, goal.Position);
+}

0e8b757928c372f14c3a91068f521594e1fb7a04
rob <109945998+rbsnz@users.noreply.github.com>
2022-10-22 01:50:05 +1300

    Implement A* algorithm

--- AstarVisualizer/Edge.cs
+++ AstarVisualizer/Edge.cs
@@ -13,19 +13,43 @@ public class Edge
     public Vertex A { get; }
     public Vertex B { get; }
 
-    public float Distance => _lineShape.Distance;
+    /// <summary>
+    /// Gets if this edge belongs to the specified vertex.
+    /// </summary>
+    public bool BelongsTo(Vertex vertex) => vertex == A || vertex == B;
 
+    /// <summary>
+    /// Gets a line that represents this edge.
+    /// </summary>
+    public Line Line => new(A.Position, B.Position);
+
+    /// <summary>
+    /// Gets or sets the line weight of this edge.
+    /// </summary>
     public float Weight
     {
         get => _lineShape.Weight;
         set => _lineShape.Weight = value;
     }
 
+    /// <summary>
+    /// Gets or sets the color of this edge.
+    /// </summary>
     public Color Color
     {
         get => _lineShape.FillColor;
         set => _lineShape.FillColor = value;
     }
+
+    /// <summary>
+    /// Gets or sets if this is a potential edge that may be inserted.
+    /// </summary>
+    public bool IsPotentialEdge { get; set; }
+
+    /// <summary>
+    /// Gets or sets if this edge is visible.
+    /// </summary>
+    public bool IsVisible { get; set; } = true;
 
     public Edge(Vertex a, Vertex b)
     {
--- AstarVisualizer/Visualizer.cs
+++ AstarVisualizer/Visualizer.cs
@@ -1,7 +1,8 @@
-﻿using SFML.Graphics;
+﻿using System.Diagnostics.CodeAnalysis;
+
+using SFML.Graphics;
 using SFML.System;
 using SFML.Window;
-using System.Diagnostics.CodeAnalysis;
 
 namespace AstarVisualizer;
 
@@ -22,14 +23,24 @@ public sealed class Visualizer
 
     private readonly List<Vertex> _vertices = new();
     private readonly List<Edge> _edges = new();
-    private readonly List<Edge> _insertableEdges = new();
+    private readonly List<Edge> _potentialEdges = new();
 
+    private readonly CircleShape _hoverCircle = new()
+    {
+        FillColor = new Color(255, 255, 255, 55),
+        Radius = VertexRadius + 10,
+        Origin = new Vector2f(VertexRadius + 10, VertexRadius + 10)
+    };
+
+    private Vertex? _hoverVertex;
     private Vertex? _draggingVertex;
     private Vector2f _dragOffset;
     private bool _canPlace;
 
     private Edge? _hoverEdge;
     private Vector2f _edgeHoverPoint;
+
+    private Vertex? start, goal;
 
     public Visualizer()
     {
@@ -44,10 +55,78 @@ public sealed class Visualizer
         _window.MouseButtonReleased += HandleMouseButtonReleased;
     }
 
-    #region Logic
-    private void CalculateInsertableEdges()
+    #region A*
+    private List<Vertex>? AStar(Vertex start, Vertex goal, HeuristicFunc h)
     {
-        _insertableEdges.Clear();
+        PriorityQueue<Vertex, float> openSet = new();
+
+        openSet.Enqueue(start, 0);
+
+        Dictionary<Vertex, Vertex> cameFrom = new();
+
+        Dictionary<Vertex, float> gScore = new();
+
+        Dictionary<Vertex, float> fScore = new();
+
+        gScore[start] = 0;
+        fScore[start] = h(start, goal);
+
+        while (openSet.Count > 0)
+        {
+            Vertex current = openSet.Dequeue();
+            if (current == goal)
+            {
+                List<Vertex> path = new() { current };
+                while (cameFrom.ContainsKey(current))
+                {
+                    current = cameFrom[current];
+                    path.Insert(0, current);
+                }
+                return path;
+            }
+
+            foreach (Vertex neighbor in current.Connections)
+            {
+                float tentativeGscore = gScore.GetValueOrDefault(current, float.PositiveInfinity)
+                    + Maths.Distance(current.Position, neighbor.Position);
+                if (tentativeGscore < gScore.GetValueOrDefault(neighbor, float.PositiveInfinity))
+                {
+                    cameFrom[neighbor] = current;
+                    gScore[neighbor] = tentativeGscore;
+                    fScore[neighbor] = tentativeGscore + h(neighbor, goal);
+                    if (!openSet.UnorderedItems.Any(x => x.Element == neighbor))
+                        openSet.Enqueue(neighbor, fScore[neighbor]);
+                }
+            }
+        }
+
+        return null;
+    }
+    #endregion
+
+    #region Logic
+    /// <summary>
+    /// Gets the vertex at the specified point.
+    /// </summary>
+    /// <param name="point">The point at which to test for a collision.</param>
+    /// <returns>The resulting vertex, or <see langword="null"/> if it does not exist.</returns>
+    private Vertex? GetVertexAtPoint(Vector2f point)
+    {
+        foreach (Vertex vertex in _vertices)
+        {
+            if (Maths.Distance(point, vertex.Position) <= vertex.Radius)
+                return vertex;
+        }
+
+        return null;
+    }
+
+    /// <summary>
+    /// Calculates potential edges between nodes that do not intersect with any existing edges.
+    /// </summary>
+    private void CalculatePotentialEdges()
+    {
+        _potentialEdges.Clear();
 
         for (int i = 0; i < _vertices.Count - 1; i++)
         {
@@ -55,17 +134,15 @@ public sealed class Visualizer
             {
                 var vertexA = _vertices[i];
                 var vertexB = _vertices[j];
-                bool valid = true;
                 if (vertexA.IsConnectedTo(vertexB))
                     continue;
-                
+
+                bool valid = true;
                 foreach (var edge in _edges)
                 {
-                    if (edge.A == vertexA || edge.A == vertexB ||
-                        edge.B == vertexA || edge.B == vertexB) continue;
+                    if (edge.BelongsTo(vertexA) || edge.BelongsTo(vertexB)) continue;
 
-                    if (Line.Intersects(new Line(edge.A.Position, edge.B.Position),
-                        new Line(vertexA.Position, vertexB.Position), out _))
+                    if (Line.Intersects(edge.Line, new Line(vertexA.Position, vertexB.Position), out _))
                     {
                         valid = false;
                         break;
@@ -75,62 +152,126 @@ public sealed class Visualizer
                 if (!valid)
                     continue;
                 
-                _insertableEdges.Add(new Edge(vertexA, vertexB) { Color = new Color(0, 0, 255, 0), Weight = 4 });
+                _potentialEdges.Add(new Edge(vertexA, vertexB) {
+                    Color = Theme.PotentialEdgeFill,
+                    Weight = 4,
+                    IsPotentialEdge = true,
+                    IsVisible = false
+                });
             }
         }
     }
 
-    private bool FindNearestEdge(IEnumerable<Edge> edges, Vector2f point, float maxDistance,
+    /// <summary>
+    /// Snaps the specified point to the nearest point along one of the specified edges,
+    /// where the distance from the point to the line does not exceed maxDistance.
+    /// </summary>
+    /// <param name="edges">The edges to search.</param>
+    /// <param name="point">The point from which to find the nearest edge.</param>
+    /// <param name="maxDistance">The maximum distance from the point t</param>
+    /// <param name="edge"></param>
+    /// <param name="intersection"></param>
+    /// <returns></returns>
+    private bool SnapPointToNearestEdge(IEnumerable<Edge> edges, Vector2f point, float maxDistance,
         [NotNullWhen(true)] out Edge? edge, out Vector2f intersection)
     {
         edge = null;
         intersection = default;
         float minDistance = float.MaxValue;
 
-        foreach (var e in edges)
+        foreach (var currentEdge in edges)
         {
-            float angle = MathF.Atan2(
-                e.B.Position.Y - e.A.Position.Y,
-                e.B.Position.X - e.A.Position.X
-            );
-            float parallelAngle = angle + MathF.PI / 2;
-            Line line = new(
-                new Vector2f(
-                    point.X + MathF.Cos(parallelAngle) * 1000,
-                    point.Y + MathF.Sin(parallelAngle) * 1000
-                ),
-                new Vector2f(
-                    point.X - MathF.Cos(parallelAngle) * 1000,
-                    point.Y - MathF.Sin(parallelAngle) * 1000
-                )
+            // Calculate a line perpendicular to the edge angle, centered around the specified point.
+            Line edgeLine = currentEdge.Line;
+            float rightAngle = edgeLine.Angle + (MathF.PI / 2);
+            Line pointLine = new(
+                point.X + MathF.Cos(rightAngle) * maxDistance,
+                point.Y + MathF.Sin(rightAngle) * maxDistance,
+                point.X - MathF.Cos(rightAngle) * maxDistance,
+                point.Y - MathF.Sin(rightAngle) * maxDistance
             );
 
-            Line edgeLine = new(e.A.Position, e.B.Position);
-            if (Line.Intersects(edgeLine, line, out Vector2f inters))
+            // Check if the lines intersect.
+            if (Line.Intersects(edgeLine, pointLine, out Vector2f p))
             {
-                float distance = Maths.Distance(point, inters);
-                if (distance <= maxDistance && distance < minDistance)
+                // Update the current edge if this is the nearest to the specified point.
+                float distance = Maths.Distance(point, p);
+                if (distance < minDistance)
                 {
-                    intersection = inters;
                     minDistance = distance;
-                    edge = e;
+                    edge = currentEdge;
+                    intersection = p;
                 }
             }
         }
 
         return edge is not null;
     }
+
+    /// <summary>
+    /// Finds a vertex nearest to the specified point where an edge
+    /// between the vertex and point would not collide with any existing edges.
+    /// </summary>
+    /// <returns>The resulting vertex if one was found, otherwise <see langword="null"/>.</returns>
+    private Vertex? FindNearestLinkableVertex(Vector2f point)
+    {
+        float minDistance = float.MaxValue;
+        Vertex? minVertex = null;
+
+        // Iterate through each existing vertex.
+        foreach (var vertex in _vertices)
+        {
+            bool valid = true;
+
+            // Create a line between the point and vertex.
+            Line line = new(vertex.Position, point);
+
+            // Iterate through each edge.
+            foreach (var edge in _edges)
+            {
+                // Skip edges connected to this vertex.
+                if (edge.A == vertex || edge.B == vertex)
+                    continue;
+
+                if (Line.Intersects(line, edge.Line, out _))
+                {
+                    valid = false;
+                    continue;
+                }
+            }
+
+            if (valid)
+            {
+                float distance = Maths.Distance(vertex.Position, point);
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    minVertex = vertex;
+                }
+            }
+        }
+
+        return minVertex;
+    }
     #endregion
 
     #region Events
     private void HandleMouseMoved(object? sender, MouseMoveEventArgs e)
     {
+        Vector2i mousePos = Mouse.GetPosition(_window);
+        Vector2f mousePosF = new(mousePos.X, mousePos.Y);
+
         if (_draggingVertex is not null)
         {
             _draggingVertex.Position = new Vector2f(e.X, e.Y) + _dragOffset;
+            _hoverCircle.Position = _draggingVertex.Position;
         }
-
-        Vector2i mousePos = Mouse.GetPosition(_window);
+        else
+        {
+            _hoverVertex = GetVertexAtPoint(mousePosF);
+            if (_hoverVertex is not null)
+                _hoverCircle.Position = _hoverVertex.Position;
+        }
 
         _canPlace = true;
 
@@ -148,16 +289,17 @@ public sealed class Visualizer
 
         if (_hoverEdge is not null)
         {
-            if (_hoverEdge.Color.B == 255)
+            if (_hoverEdge.IsPotentialEdge)
             {
-                _hoverEdge.Color = new Color(0, 0, 255, 0);
+                _hoverEdge.IsVisible = false;
             }
         }
         _hoverEdge = null;
 
-        Vector2f mousePosF = new(mousePos.X, mousePos.Y);
-        if (FindNearestEdge(_edges, mousePosF, 10.0f, out Edge? edge, out Vector2f intersection) ||
-            FindNearestEdge(_insertableEdges, mousePosF, 30.0f, out edge, out intersection))
+        if (!Keyboard.IsKeyPressed(Keyboard.Key.LShift)) return;
+
+        if (SnapPointToNearestEdge(_edges, mousePosF, 10.0f, out Edge? edge, out Vector2f intersection) ||
+            SnapPointToNearestEdge(_potentialEdges, mousePosF, 30.0f, out edge, out intersection))
         {
             float distanceMouse = Maths.Distance(mousePosF, intersection);
             if (distanceMouse <= VertexRadius * 2)
@@ -166,14 +308,13 @@ public sealed class Visualizer
 
                 float distanceA = Maths.Distance(edge.A.Position, intersection);
                 float distanceB = Maths.Distance(edge.B.Position, intersection);
-                if (//distanceMouse <= 10 &&
-                    distanceA > edge.A.Radius * 2 &&
+                if (distanceA > edge.A.Radius * 2 &&
                     distanceB > edge.B.Radius * 2)
                 {
                     _hoverEdge = edge;
-                    if (_hoverEdge.Color.B == 255)
+                    if (_hoverEdge.IsPotentialEdge)
                     {
-                        _hoverEdge.Color = new Color(0, 0, 255, 128);
+                        _hoverEdge.IsVisible = true;
                     }
                     _edgeHoverPoint = intersection;
                 }
@@ -183,18 +324,59 @@ public sealed class Visualizer
 
     private void HandleMouseButtonPressed(object? sender, MouseButtonEventArgs e)
     {
+        Vector2f mousePosF = new(e.X, e.Y);
+        Vertex? hoverVertex = GetVertexAtPoint(mousePosF);
+        if (hoverVertex != null && e.Button == Mouse.Button.Right)
+        {
+            if (goal is null)
+            {
+                if (start is null)
+                {
+                    foreach (var edge in _edges)
+                    {
+                        edge.Color = new Color(0, 255, 0);
+                    }
+                    start = hoverVertex;
+                    return;
+                }
+                else if (start == hoverVertex)
+                {
+                    return;
+                }
+
+                goal = hoverVertex;
+
+                List<Vertex>? path = AStar(start, goal, Heuristics.Euclidean);
+                if (path is not null)
+                {
+                    for (int i = 0; i < path.Count - 1; i++)
+                    {
+                        Vertex next = path[i + 1];
+                        Edge pathEdge = path[i].Edges.First(e => e.A == next || e.B == next);
+                        pathEdge.Color = new Color(255, 0, 255);
+                    }
+                }
+
+                start = null;
+                goal = null;
+            }
+        }
+
         if (e.Button != Mouse.Button.Left)
             return;
 
+        Edge? newEdge;
+
         if (_hoverEdge is not null)
         {
-            if (_hoverEdge.Color.B == 255)
+            if (_hoverEdge.IsPotentialEdge)
             {
-                if (_hoverEdge.A.Connect(_hoverEdge.B, out Edge? newEdge))
+                if (_hoverEdge.A.Connect(_hoverEdge.B, out newEdge))
                 {
+                    newEdge.Color = Theme.EdgeFill;
                     _edges.Add(newEdge);
                     _hoverEdge = null;
-                    CalculateInsertableEdges();
+                    CalculatePotentialEdges();
                     return;
                 }
             }
@@ -209,7 +391,7 @@ public sealed class Visualizer
                         _vertices.Remove(_hoverEdge.B);
                     _hoverEdge = null;
 
-                    CalculateInsertableEdges();
+                    CalculatePotentialEdges();
                     return;
                 }
 
@@ -218,18 +400,12 @@ public sealed class Visualizer
 
         double minDistance = double.MaxValue;
 
-        Vector2f mousePos = new(e.X, e.Y);
-        foreach (var vertex in _vertices)
+        _draggingVertex = GetVertexAtPoint(mousePosF);
+        if (_draggingVertex is not null)
         {
-            float distance = Maths.Distance(mousePos, vertex.Position);
-            if (distance < minDistance)
-                minDistance = distance;
-            if (distance <= vertex.Radius)
-            {
-                _draggingVertex = vertex;
-                _dragOffset = vertex.Position - mousePos;
-                return;
-            }
+            _canPlace = true;
+            _dragOffset = _draggingVertex.Position - mousePosF;
+            return;
         }
 
         if (!_canPlace) return;
@@ -237,14 +413,19 @@ public sealed class Visualizer
         // Prevent placing vertices too close to each other.
         if (minDistance <= (VertexRadius * 3)) return;
 
+        Vertex? linkVertex = FindNearestLinkableVertex(new Vector2f(e.X, e.Y));
+        if (linkVertex is null && _vertices.Count > 0)
+            return;
+
         // Create a new vertex at the mouse location.
         var newVertex = new Vertex(new Vector2f(e.X, e.Y), VertexRadius);
 
         // Connect the last added vertex to the new vertex, if it exists.
-        if (_vertices.Count > 0 && _vertices[^1].Connect(newVertex, out Edge? edge))
+        if (linkVertex is not null &&
+            linkVertex.Connect(newVertex, out newEdge))
         {
             // Add the newly created edge.
-            _edges.Add(edge);
+            _edges.Add(newEdge);
         }
 
         // Add the new vertex.
@@ -259,11 +440,11 @@ public sealed class Visualizer
             _draggingVertex = null;
         }
 
-        CalculateInsertableEdges();
+        CalculatePotentialEdges();
     }
     #endregion
 
-    #region Event loop
+    #region Main loop
     public void Run()
     {
         while (_window.IsOpen)
@@ -276,16 +457,17 @@ public sealed class Visualizer
 
     private void Update()
     {
-        Time delta = _clock.Restart();
+        _hoverCircle.FillColor = (_draggingVertex is not null) ? (_canPlace ? Theme.VertexDragging : Theme.VertexDraggingInvalid) : Theme.VertexHover;
     }
 
     private void Draw()
     {
         _window.Clear(Theme.Background);
 
-        foreach (var edge in _insertableEdges)
+        foreach (var edge in _edges.Concat(_potentialEdges))
         {
-            edge.Draw(_window);
+            if (edge.IsVisible)
+                edge.Draw(_window);
         }
 
         if (_hoverEdge is not null)
@@ -306,6 +488,9 @@ public sealed class Visualizer
         {
             vertex.Draw(_window);
         }
+
+        if (_hoverVertex is not null)
+            _window.Draw(_hoverCircle);
 
         _window.Display();
     }
